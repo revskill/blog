a59bd7e778f12953ca55f27447dcf6729b98ef4a
revskill <dungth@hpu.edu.vn>
2011-05-31 07:46:55 +0700

    Initial blog commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f0071112578695510d0d53218b51070d34ee6d6134cfe047c3a6b49b10a125fe705876b6e1eb7e078ac806c9f96d0610385623549565584f781b918a9b4768c2'
